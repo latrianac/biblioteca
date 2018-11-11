b787e0580c5d80547eb1a8282baf9d1a0c988806
latrianac <lonardo1990@gmail.com>
2018-11-11 16:52:42 -0500

    three

--- apps/libro/models.py
+++ apps/libro/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class libro(models.Model):
+    titulo = models.CharField(max_length=250)
+    subtitulo = models.CharField(max_length=250)
+    autor = models.CharField(max_length=250)
+    categoria = models.CharField(max_length=250)
+    fecha_publicacion = models.DateField()
+    editor = models.CharField(max_length=250)
+    descripcion = models.CharField(max_length=250)
+    image = models.FileField(upload_to='post_image', blank = True)
+
