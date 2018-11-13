4d81edf78fd28dd0d5b0a334cf90f1f44e2b13cf
latrianac <lonardo1990@gmail.com>
2018-11-13 17:37:32 -0500

    eleven

--- apps/libro/urls.py
+++ apps/libro/urls.py
@@ -3,7 +3,7 @@ from apps.libro.views import index, libro_view, libro_list, libro_edit, libro_de
 
 app_name = 'libro'
 urlpatterns = [
-    path('', index, name= 'inicio'),
+    path('', index, name='inicio'),
     path('nuevo/', libro_view, name='libro_crear'),
     path('listado/', libro_list, name='libro_listar'),
     re_path(r'^editar/(?P<id_libro>\w+)$', libro_edit, name='libro_editar'),
