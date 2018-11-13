950afe3366bc3882adb0f56ef710ca79c3d8c395
latrianac <lonardo1990@gmail.com>
2018-11-13 15:41:43 -0500

    siete

--- apps/libro/urls.py
+++ apps/libro/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
-from apps.libro.views import index, libro_view
+from django.urls import path, re_path
+from apps.libro.views import index, libro_view, libro_list, libro_edit
+
 
 urlpatterns = [
     path('', index, name='index'),
     path('nuevo/', libro_view, name='libro_crear'),
+    path('listado/', libro_list, name='libro_listar'),
+    re_path(r'^editar/(?P<id_libro>\w+)$', libro_edit, name='libro_editar'),
 ]
--- apps/libro/views.py
+++ apps/libro/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from apps.libro.forms import LibroForm
+from django.views.generic import ListView
+from apps.libro.models import Libro
 
 def index(request):
     return render(request, 'libro/index.html')
@@ -15,3 +17,25 @@ def libro_view(request):
 
     return render(request, 'libro/libro_form.html', {'form': form})
 
+
+def libro_list(request):
+    libro = Libro.objects.all()
+    contexto = {'libros':libro}
+    return render(request, 'libro/libro_list.html', contexto)
+
+def libro_edit(request, id_libro):
+    libro = Libro.objects.get(id=id_libro)
+    if request.method == 'GET':
+        form = LibroForm(instance=libro)
+    else:
+        form = LibroForm(request.POST, instance=libro)
+        if form.is_valid():
+            form.save()
+        return redirect('libro_listar')
+    return render(request, 'libro/libro_form.html', {'form': form})
+
+
+
+#class LibroList(ListView):
+#    model = Libro
+#    template_name = 'libro/libro_list.html'
