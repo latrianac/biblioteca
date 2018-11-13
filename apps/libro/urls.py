6f7b5c8a163f279ef4b0bf6f6a6a6d896ca8ce1a
latrianac <lonardo1990@gmail.com>
2018-11-13 17:30:54 -0500

    ten

--- apps/libro/urls.py
+++ apps/libro/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, re_path
-from apps.libro.views import index, libro_view, libro_list, libro_edit
+from apps.libro.views import index, libro_view, libro_list, libro_edit, libro_delete
 
 app_name = 'libro'
 urlpatterns = [
@@ -7,4 +7,5 @@ urlpatterns = [
     path('nuevo/', libro_view, name='libro_crear'),
     path('listado/', libro_list, name='libro_listar'),
     re_path(r'^editar/(?P<id_libro>\w+)$', libro_edit, name='libro_editar'),
+    re_path(r'^eliminar/(?P<id_libro>\w+)$', libro_delete, name='libro_eliminar'),
 ]
--- apps/libro/views.py
+++ apps/libro/views.py
@@ -32,7 +32,12 @@ def libro_edit(request, id_libro):
         return redirect('libro_listar')
     return render(request, 'libro/libro_form.html', {'form': form})
 
-
+def libro_delete(request, id_libro):
+    libro = Libro.objects.get(id=id_libro)
+    if request.method == 'POST':
+        libro.delete()
+        return redirect('libro:libro_listar')
+    return render(request, 'libro/libro_delete.html', {'libro': libro})
 
 
 
