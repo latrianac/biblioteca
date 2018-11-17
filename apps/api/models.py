a6777d21db42d944278e529bb6872084f30c506f
latrianac <lonardo1990@gmail.com>
2018-11-17 11:18:56 -0500

    20

--- apps/api/models.py
+++ apps/api/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 
+# Modelo que guarda las busquedas realizadas por el usuario
 class LibroBusquedaGoogle(models.Model):
     titulo = models.CharField(max_length=250)
 
     def __str__(self):
         return self.titulo
+
+
+# Modelo que guarda los id buscador por el usuario
 
 class LibroGuardarGoogle(models.Model):
     identificacion = models.CharField(max_length=250)
